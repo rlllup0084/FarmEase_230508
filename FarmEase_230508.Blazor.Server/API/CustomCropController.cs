7a9467bb1f53182976f329e4f3c4e90cc4643c3b
rlllup0084 <llup.renee@gmail.com>
2023-05-16 21:11:44 +0800

    Done related api

--- FarmEase_230508.Blazor.Server/API/CustomCropController.cs
+++ FarmEase_230508.Blazor.Server/API/CustomCropController.cs
@@ -24,10 +24,11 @@ namespace FarmEase_230508.Blazor.Server.API {
         public IActionResult GetCropTasksByCropId(Guid cropId) {
             using IObjectSpace newObjectSpace = objectSpaceFactory.CreateObjectSpace<CropTask>();
             var cropTasks = newObjectSpace.GetObjects<CropTask>(CriteriaOperator.Parse("[CropId] = ?", cropId));
-            var tasks = cropTasks.Select(o => new Tasks {
+            var tasks = cropTasks.OrderBy(o=>o.MainSeq).Select(o => new Tasks {
                 CropId = o.CropId.Oid,
                 ParentId = o.ParentId != null ? o.ParentId.Oid : null,
                 Seq = o.Seq,
+                MainSeq = o.MainSeq,
                 RecType = o.RecType,
                 RecValue = o.RecValue,
                 Title = o.Title,
@@ -36,6 +37,43 @@ namespace FarmEase_230508.Blazor.Server.API {
                 Days = o.Days
             }).ToList();
             return Ok(tasks);
+        }
+
+        [HttpGet("/CustomCropTasks/CropCycles")]
+        public IActionResult GetCropCycles() {
+            ISecurityStrategyBase security = securityProvider.GetSecurity();
+            var user = security.User;
+            using IObjectSpace newObjectSpace = objectSpaceFactory.CreateObjectSpace<CropCycle>();
+            ApplicationUser applicationUser = newObjectSpace.GetObject((ApplicationUser)user);
+            var cropCycles = newObjectSpace.GetObjects<CropCycle>(CriteriaOperator.Parse("[Owner] = ?", applicationUser));
+            var cycles = cropCycles.Select(o => new CropCycleModel {
+                CropCycleId = o.Oid,
+                Owner = o.Owner.UserName,
+                CropId = o.CropId.Oid,
+                StartDate = o.StartDate
+            }).ToList();
+            return Ok(cycles);
+        }
+
+        [HttpGet("/CustomCropTasks/CropCycleTasks/{cropCycleId}")]
+        public IActionResult GetCropCyclesTaskByCycleId(int cropCycleId) {
+            using IObjectSpace newObjectSpace = objectSpaceFactory.CreateObjectSpace<CropCycleTask>();
+            var cropCycles = newObjectSpace.GetObjects<CropCycleTask>(CriteriaOperator.Parse("[CropCycleId] = ?", cropCycleId));
+            var cyclesTasks = cropCycles.OrderBy(o => o.MainSeq).Select(o => new CropCycleTaskModel {
+                CropCycleId = o.CropCycleId.Oid,
+                TaskId = o.CropCycleId.Oid,
+                ParentId = o.ParentId != null ? o.ParentId.Oid : null,
+                Title = o.Title,
+                Description = o.Description,
+                StartDate = o.StartDate,
+                EndDate = o.EndDate,
+                Seq = o.Seq,
+                MainSeq = o.MainSeq,
+                RecType = o.RecType,
+                RecValue = o.RecValue,
+                Status = o.Status
+            }).ToList();
+            return Ok(cyclesTasks);
         }
 
         [HttpPost("/CustomCropTasks/CropCycle/Create")]
@@ -89,12 +127,35 @@ namespace FarmEase_230508.Blazor.Server.API {
         public Guid CropId { get; set; }
         public int? ParentId { get; set; }
         public int Seq { get; set; }
+        public int MainSeq { get; set; }
         public RecurrenceType RecType { get; set; }
         public int RecValue { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
         public string Notes { get; set; }
         public int Days { get; set; }
+    }
+
+    public class CropCycleTaskModel {
+        public int CropCycleId { get; set; }
+        public int TaskId { get; set; }
+        public int? ParentId { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public int Seq { get; set; }
+        public int MainSeq { get; set; }
+        public RecurrenceType RecType { get; set; }
+        public int RecValue { get; set; }
+        public CropCycleTaskStatus Status { get; set; }
+    }
+
+    public class CropCycleModel {
+        public int CropCycleId { get; set; }
+        public string Owner { get; set; }
+        public Guid CropId { get; set; }
+        public DateTime StartDate { get; set; }
     }
 
     public class CreateCropCycleCommand {
