b70da9d9425b2ba5ca26665539e2ce010f06fcde
rlllup0084 <llup.renee@gmail.com>
2023-05-17 22:04:15 +0800

    2305172204

--- FarmEase_230508.Maui/FarmEase_230508.Maui/Views/CropIdToImageConverter.cs
+++ FarmEase_230508.Maui/FarmEase_230508.Maui/Views/CropIdToImageConverter.cs
@@ -7,9 +7,9 @@ public class CropIdToImageConverter : IValueConverter {
         if (value.ToString() is string status) {
             switch (status) {
                 case "8eba8dea-9414-44b5-a83f-37262d4c3fcb":
-                    return "hold.png"; // replace with your current status image file name
+                    return "corn.png"; // replace with your current status image file name
                 case "4cdc549d-d5dc-4440-ad87-530380401d35":
-                    return "release.png"; // replace with your approve status image file name
+                    return "cassava.png"; // replace with your approve status image file name
                                           // add more cases for other status values if needed
             }
         }
